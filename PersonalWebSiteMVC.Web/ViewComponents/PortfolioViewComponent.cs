6cbcd544ab5819da452bb29d1cc2c89181592c1c
Osman Topuz <osmantopuz98@gmail.com>
2024-05-13 17:24:55 +0300

    portfolio and reference view comp.

---
+++ PersonalWebSiteMVC.Web/ViewComponents/PortfolioViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonalWebSiteMVC.Service.Services.Abstractions;
+
+namespace PersonalWebSiteMVC.Web.ViewComponents
+{
+    public class PortfolioViewComponent : ViewComponent
+    {
+        private readonly IPortfolioService portfolioService;
+
+        public PortfolioViewComponent(IPortfolioService portfolioService)
+        {
+            this.portfolioService = portfolioService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var portfolios = await portfolioService.GetAllPortfolioWithImageAsync();
+            return View(portfolios);
+        }
+    }
+}
---
+++ PersonalWebSiteMVC.Web/ViewComponents/TestimonialViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonalWebSiteMVC.Service.Services.Abstractions;
+
+namespace PersonalWebSiteMVC.Web.ViewComponents
+{
+    public class TestimonialViewComponent : ViewComponent
+    {
+        private readonly ITestimonialService testimonialService;
+
+        public TestimonialViewComponent(ITestimonialService testimonialService)
+        {
+            this.testimonialService = testimonialService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var testimonials = await testimonialService.GetAllTestimonialsAsync();
+            return View(testimonials);
+        }
+    }
+}
