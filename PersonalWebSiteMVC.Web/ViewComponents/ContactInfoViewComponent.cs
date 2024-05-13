e5f5ae5f54e99280ce91b0b153108fcd313e933b
Osman Topuz <osmantopuz98@gmail.com>
2024-05-13 18:10:14 +0300

    contact info component

---
+++ PersonalWebSiteMVC.Web/ViewComponents/ContactInfoViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonalWebSiteMVC.Service.Services.Abstractions;
+
+namespace PersonalWebSiteMVC.Web.ViewComponents
+{
+    public class ContactInfoViewComponent : ViewComponent
+    {
+        private readonly IUserService userService;
+
+        public ContactInfoViewComponent(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = userService.GetFrontProfile();
+
+            return View(user);
+
+        }
+    }
+}
