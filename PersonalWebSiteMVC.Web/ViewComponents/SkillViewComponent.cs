a128bb53b53fe3923d4472c50bb8ef0159565468
Osman Topuz <osmantopuz98@gmail.com>
2024-05-13 14:36:48 +0300

    section view component

---
+++ PersonalWebSiteMVC.Web/ViewComponents/SkillViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonalWebSiteMVC.Service.Services.Abstractions;
+
+namespace PersonalWebSiteMVC.Web.ViewComponents
+{
+    public class SkillViewComponent : ViewComponent
+    {
+        private readonly ITalentService talentService;
+
+        public SkillViewComponent(ITalentService talentService)
+        {
+            this.talentService = talentService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var skills = await talentService.GetAllTalentsWithImageAsync();
+            return View(skills);
+        }
+    }
+}
